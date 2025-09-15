5b486d0d0055b78103291881e2c4e5cdd0e5bde9
Dmytro Tkachenko <dmitry.tkachenko@dataart.com>
2025-09-15 12:51:40 +0300

    Initial commit from Sanity CLI

---
+++ src/sanity/schemaTypes/modules/testimonial.featured.ts
@@ -0,0 +1,30 @@
+import { defineField, defineType } from 'sanity'
+import { GrBlockQuote } from 'react-icons/gr'
+import { getBlockText } from 'sanitypress-utils'
+
+export default defineType({
+	name: 'testimonial.featured',
+	title: 'Testimonial (featured)',
+	icon: GrBlockQuote,
+	type: 'object',
+	fields: [
+		defineField({
+			name: 'testimonial',
+			type: 'reference',
+			to: [{ type: 'testimonial' }],
+		}),
+	],
+	preview: {
+		select: {
+			testimonial: 'testimonial.content',
+			image: 'testimonial.author.image',
+		},
+		prepare: ({ testimonial, image }) => {
+			return {
+				title: getBlockText(testimonial),
+				subtitle: 'Testimonial (featured)',
+				media: image,
+			}
+		},
+	},
+})
